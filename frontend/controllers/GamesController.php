4e8465656e5c75661f3cd8b23a5e651589f4dc14
Micol <micolpa08@gmail.com>
2019-11-26 13:57:09 -0400

    probando

--- frontend/controllers/GamesController.php
+++ frontend/controllers/GamesController.php
@@ -10,7 +10,7 @@ use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
 use yii\web\UploadedFile;
 use yii\data\ActiveDataProvider;
-
+ 
 /**
  * GamesController implements the CRUD actions for Games model.
  */
