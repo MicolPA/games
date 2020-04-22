bc097789e4e33787365f92cd577d79a10ac67041
Micol <micolpa08@gmail.com>
2020-04-22 12:04:22 -0400

    btn

--- frontend/views/games/juego.php
+++ frontend/views/games/juego.php
@@ -27,6 +27,12 @@
 			
 			<img src="<?php echo Yii::getAlias("@web") .'/'. $model->portada_out; ?>" class="d-block w-100" alt="<?php echo $model->name ?>" width='100%'>
 			<div class="mt-4">
+				<div class="mb-2">
+					<div style="width:100%;text-align:center;margin: 20px 0 0 0;">
+					<a href="https://vardes.xyz/r/?token=350d5db553b836aa394da6379df20bf3c3fb32b1&q=descargar-juego" class="btn btn-success btn-lg font-weight-bold" style="display: inline-block;margin-right: 20px;padding:5px;width:35%;">DESCARGA RÁPIDA</a>
+					<a href="https://vardes.xyz/r/?token=350d5db553b836aa394da6379df20bf3c3fb32b1&q=descargar-juego" class="btn btn-success btn-lg font-weight-bold" style="display: inline-block;margin-right: 20px;padding:5px;width:35%;">DESCARGA AQUÍ</a>
+					</div>
+				</div>
 				<div class="pb-3 pt-3 pl-0">
 					<span class="display-4 text-primary font-weight-b title" >Descripción</span>
 					<hr>
